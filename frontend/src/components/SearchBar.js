34b872552713db0e1542798d6716beb0a42506d5
Ajaykumar HP <104970762+Ajhp18@users.noreply.github.com>
2024-05-13 18:14:01 +0530

    Searchbar and daterange filter component added

---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { IconButton, InputAdornment, TextField } from '@mui/material'
+import ClearIcon from '@mui/icons-material/Clear';
+
+export default function SearchBar({ placeholder, searchInputText, onChangeOfSearchInput, onClearSearchInput }) {
+
+  return (
+
+    //Note:While using this prop names in another page, Use the same prop names. Otherwise it will throw the errors
+
+    <TextField
+      variant='outlined'
+      sx={{ width: '50%' }}
+      placeholder={placeholder}
+      value={searchInputText}
+      onChange={onChangeOfSearchInput}
+      InputProps={{
+        endAdornment: (
+          <IconButton onClick={onClearSearchInput}>
+            <ClearIcon />
+          </IconButton>
+        )
+      }}
+    />
+  )
+}
