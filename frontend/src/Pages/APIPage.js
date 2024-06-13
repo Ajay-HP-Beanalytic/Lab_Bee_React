c59dda3e60910a185661a20d8d00ccf4d64d317b
kiran-bea <kiran.j@beanalytic.com>
2024-06-13 16:53:59 +0530

    changes api

--- frontend/src/Pages/APIPage.js
+++ frontend/src/Pages/APIPage.js
@@ -1,6 +1,6 @@
 // Api's used in quotations and Quotations Dashboard page:
 
 // const serverBaseAddress = `http://localhost:4000`;
-const serverBaseAddress = "https://labbee.beanalytic.com:4000"; //Hostinger IP
+const serverBaseAddress = "https://labbee.beanalytic.com/api"; //Hostinger IP
 
 export { serverBaseAddress };
