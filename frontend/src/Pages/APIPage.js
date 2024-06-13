c5940640e4a6e36c108509526555c2df40c4e611
kiran-bea <kiran.j@beanalytic.com>
2024-06-13 14:52:24 +0530

    changed Ip to domain

--- frontend/src/Pages/APIPage.js
+++ frontend/src/Pages/APIPage.js
@@ -1,6 +1,6 @@
 // Api's used in quotations and Quotations Dashboard page:
 
 // const serverBaseAddress = `http://localhost:4000`;
-const serverBaseAddress = "http://194.238.16.142:4000"; //Hostinger IP
+const serverBaseAddress = "http://labbee.beanalytic.com:4000"; //Hostinger IP
 
 export { serverBaseAddress };
