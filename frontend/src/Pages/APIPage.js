4b5a182e2b2b631db75dbe89901f0792d7eb0578
kiran-bea <kiran.j@beanalytic.com>
2024-06-13 14:48:56 +0530

    changed hostinger IP

--- frontend/src/Pages/APIPage.js
+++ frontend/src/Pages/APIPage.js
@@ -1,6 +1,6 @@
 // Api's used in quotations and Quotations Dashboard page:
 
-const serverBaseAddress = `http://localhost:4000`;
-// const serverBaseAddress = "http://192.168.68.162:4000";
+// const serverBaseAddress = `http://localhost:4000`;
+const serverBaseAddress = "http://194.238.16.142:4000"; //Hostinger IP
 
 export { serverBaseAddress };
