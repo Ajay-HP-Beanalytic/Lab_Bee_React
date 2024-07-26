bb002be90597612c0f82851bb476d9ae5b24962a
Aj-thinks <ajay.h@beanalytic.com>
2024-07-26 15:26:48 +0530

    Back to Dvelopment

--- frontend/src/Pages/APIPage.js
+++ frontend/src/Pages/APIPage.js
@@ -1,6 +1,6 @@
 // Api's used in quotations and Quotations Dashboard page:
 
-// const serverBaseAddress = `http://localhost:4000`;
-const serverBaseAddress = "https://labbee.beanalytic.com:4002"; //Hostinger IP
+const serverBaseAddress = `http://localhost:4000`;
+//const serverBaseAddress = "https://labbee.beanalytic.com:4002"; //Hostinger IP
 
 export { serverBaseAddress };
